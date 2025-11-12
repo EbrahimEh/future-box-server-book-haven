a96f3ecb6dbf4dc6cc7c9a43fd9a2a67cad22aec
EbrahimEh <mreh017@gmail.com>
2025-11-12 23:26:40 +0600

    Successfully Deploy In Vercel

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         //
         const db = client.db('bookHaven');
@@ -106,7 +106,7 @@ async function run() {
 
 
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
     finally {
